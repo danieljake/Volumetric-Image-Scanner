0f44d32933ac4ebf2f10f933e04184a607e23cb6
Daniel <srvdan001@myuct.ac.za>
2019-03-13 13:55:05 +0200

    Initial Commit

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 2
+//
+//  Created by Daniel Servant on 2019/03/13.
+//  Copyright © 2019 Daniel Servant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
